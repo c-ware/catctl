6f6e1d6df6b8357be0200cfdacadebd98378287e
HackerSmacker <wec@m1-laptop.local>
2022-06-04 13:27:18 -0500

    feat: macro-ify mode changing

--- src/lib/catctl_mode.c
+++ src/lib/catctl_mode.c
@@ -3,35 +3,27 @@
 #include <string.h>
 #include <catctl.h>
 
+#define CATCTL_MODE(name, retMacName) \
+    if(strcmp(mode, name) == 0) \
+        return CAT_MODE_ ## retMacName;
 
 char* catctl_string_mode(char* mode) {
-    if(strcmp(mode, "fm") == 0) 
-        return CAT_MODE_FM;
-    else if(strcmp(mode, "am") == 0)
-        return CAT_MODE_AM;
-    else if(strcmp(mode, "lsb") == 0)
-        return CAT_MODE_LSB;
-    else if(strcmp(mode, "usb") == 0)
-        return CAT_MODE_USB;
-    else if(strcmp(mode, "cw") == 0)
-        return CAT_MODE_CW_LSB;
-    else if(strcmp(mode, "cw-lsb") == 0)
-        return CAT_MODE_CW_LSB;
-    else if(strcmp(mode, "cw-usb") == 0)
-        return CAT_MODE_CW_USB;
-    else if(strcmp(mode, "rtty") == 0)
-        return CAT_MODE_RTTY_USB;
-    else if(strcmp(mode, "rtty-usb") == 0)
-        return CAT_MODE_RTTY_USB;
-    else if(strcmp(mode, "rtty-lsb") == 0)
-        return CAT_MODE_RTTY_LSB;
-    else if(strcmp(mode, "am-nar") == 0)
-        return CAT_MODE_AM_NAR;
-    else if(strcmp(mode, "fm-nar") == 0)
-        return CAT_MODE_FM_NAR;
-    else if(strcmp(mode, "c4fm") == 0)
-        return CAT_MODE_C4FM;
-
+    CATCTL_MODE("fm", FM)
+    CATCTL_MODE("am", AM)
+    CATCTL_MODE("lsb", LSB)
+    CATCTL_MODE("usb", USB)
+    CATCTL_MODE("cw", CW_USB)
+    CATCTL_MODE("cw-usb", CW_USB)
+    CATCTL_MODE("cw-lsb", CW_LSB)
+    CATCTL_MODE("rtty", RTTY_USB)
+    CATCTL_MODE("rtty-usb", RTTY_USB)
+    CATCTL_MODE("rtty-lsb", RTTY_LSB)
+    CATCTL_MODE("data", DATA_USB)
+    CATCTL_MODE("data-lsb", DATA_LSB)
+    CATCTL_MODE("data-usb", DATA_USB)
+    CATCTL_MODE("am-nar", AM_NAR)
+    CATCTL_MODE("fm-nar", FM_NAR)
+    CATCTL_MODE("c4fm", C4FM)
     return "?";
 }
     
